7b2466ade624cf78fb3bb38ce59c6f3611ec4440
tomhon <tomhon@hotmail.com>
2017-09-06 16:40:27 -0700

    added basic files

---
+++ config.js
@@ -0,0 +1,71 @@
+var config = {}
+
+config.endpoint = "https://suplayerhealth.documents.azure.com:443/";
+config.primaryKey = "iR2sJ04BfMURMgIxKPA6fQz9YFIUfRcauueQG3TthlakwAZvz4dyRYrodr1iZMh4oM3TM9Uh38QSDPL7az1mBg==";
+
+config.database = {
+    "id": "PlayerHealthDB"
+};
+
+config.collection = {
+    "id": "G04Copa2017"
+};
+
+config.documents = {
+    "Andersen": {
+        "id": "Anderson.1",
+        "lastName": "Andersen",
+        "parents": [{
+            "firstName": "Thomas"
+        }, {
+                "firstName": "Mary Kay"
+            }],
+        "children": [{
+            "firstName": "Henriette Thaulow",
+            "gender": "female",
+            "grade": 5,
+            "pets": [{
+                "givenName": "Fluffy"
+            }]
+        }],
+        "address": {
+            "state": "WA",
+            "county": "King",
+            "city": "Seattle"
+        }
+    },
+    "Wakefield": {
+        "id": "Wakefield.7",
+        "parents": [{
+            "familyName": "Wakefield",
+            "firstName": "Robin"
+        }, {
+                "familyName": "Miller",
+                "firstName": "Ben"
+            }],
+        "children": [{
+            "familyName": "Merriam",
+            "firstName": "Jesse",
+            "gender": "female",
+            "grade": 8,
+            "pets": [{
+                "givenName": "Goofy"
+            }, {
+                    "givenName": "Shadow"
+                }]
+        }, {
+                "familyName": "Miller",
+                "firstName": "Lisa",
+                "gender": "female",
+                "grade": 1
+            }],
+        "address": {
+            "state": "NY",
+            "county": "Manhattan",
+            "city": "NY"
+        },
+        "isRegistered": false
+    }
+};
+
+module.exports = config;
